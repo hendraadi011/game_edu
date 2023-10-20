cf77df292477c16e954d7a32e0fd42997081e672
ilhamdwikarifani <ilhamdwiki0408@gmail.com>
2023-10-20 13:40:02 +0700

    update text + total sweper game menu

--- js/materi.js
+++ js/materi.js
@@ -1,64 +1,95 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 1,
-    spaceBetween: 40,
-    // slidesPerGroup: 3,
-    loop: true,
-    // loopFillGroupWithBlank: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  // slidesPerGroup: 3,
+  loop: true,
+  // loopFillGroupWithBlank: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-        950: {
-            slidesPerView: 4,
-        },
-
-
+    950: {
+      slidesPerView: 3,
     },
+    950: {
+      slidesPerView: 4,
+    },
+  },
 });
 
-// toggle class active 
+// game Sweper
+var swiper_game = new Swiper(".slide-content-game", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  // slidesPerGroup: 3,
+  loop: true,
+  // loopFillGroupWithBlank: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination-game",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-game",
+    prevEl: ".swiper-button-prev-game",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 2,
+    },
+  },
+});
 
-const navbarNav = document.querySelector('.navbar-nav');
+// toggle class active
 
+const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
 
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle('active');
-    console.log('halo')
-}
+  navbarNav.classList.toggle("active");
+  console.log("halo");
+};
 
 //klik di luar sidebar untuk menghilangkan nav
 
-const hamburger = document.querySelector('#hamburger-menu');
+const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
-})
-
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
 
 const materiSatu = () => {
-    // e.preventDefault();
-    // window.location('materi/materi-satu.html')
-    window.location.href = '../materi/materi-satu.html'
-}
+  // e.preventDefault();
+  // window.location('materi/materi-satu.html')
+  window.location.href = "../materi/materi-satu.html";
+};
